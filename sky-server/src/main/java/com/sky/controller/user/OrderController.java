83b0315664e5ad64df78068f78fd376263b125ec
Yokior <844599449@qq.com>
2023-08-01 15:49:50 +0800

    用户层_魔改微信支付_直接支付成功

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,17 +1,25 @@
 package com.sky.controller.user;
 
+import com.alibaba.druid.support.json.JSONUtils;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.service.ShoppingCartService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.UUID;
 
 @RestController("userOrderController")
 @RequestMapping("/user/order")
@@ -23,11 +31,49 @@ public class OrderController
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 用户下单
+     *
+     * @param ordersSubmitDTO
+     * @return
+     */
     @PostMapping("/submit")
     @ApiOperation("用户下单")
     public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO)
     {
-        OrderSubmitVO orderSubmitVO = orderService.sumbitOrder(ordersSubmitDTO);
+        log.info("用户下单：{}", ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
         return Result.success(orderSubmitVO);
     }
+
+    /**
+     * 魔改版 订单支付 直接成功
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @PutMapping("/payment")
+    @ApiOperation("订单支付")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception
+    {
+        log.info("订单支付：{}", ordersPaymentDTO);
+        orderService.paySuccess(ordersPaymentDTO);
+
+        // 清除购物车
+        shoppingCartService.cleanShoppingCart();
+
+        // 生成假的数据
+        OrderPaymentVO paymentVO = OrderPaymentVO.builder()
+                .nonceStr(UUID.randomUUID().toString())
+                .paySign(UUID.randomUUID().toString())
+                .timeStamp(String.valueOf(System.currentTimeMillis()))
+                .signType("lalala")
+                .packageStr("lalala")
+                .build();
+        return Result.success(paymentVO);
+    }
+
 }
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,21 +1,19 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
-import com.sky.entity.AddressBook;
-import com.sky.entity.OrderDetail;
-import com.sky.entity.Orders;
-import com.sky.entity.ShoppingCart;
+import com.sky.entity.*;
 import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.OrderBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
-import com.sky.mapper.AddressBookMapper;
-import com.sky.mapper.OrderDetailMapper;
-import com.sky.mapper.OrderMapper;
-import com.sky.mapper.ShoppingCartMapper;
+import com.sky.mapper.*;
 import com.sky.service.AddressBookService;
 import com.sky.service.OrderService;
 import com.sky.service.ShoppingCartService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +21,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -43,6 +42,9 @@ public class OrderServiceImpl implements OrderService
     @Autowired
     private ShoppingCartMapper shoppingCartMapper;
 
+    @Autowired
+    private UserMapper userMapper;
+
     /**
      * 用户下单
      * @param ordersSubmitDTO
@@ -50,7 +52,7 @@ public class OrderServiceImpl implements OrderService
      */
     @Override
     @Transactional
-    public OrderSubmitVO sumbitOrder(OrdersSubmitDTO ordersSubmitDTO)
+    public OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO)
     {
         // 处理各种业务异常
         // 地址簿为空
@@ -109,4 +111,75 @@ public class OrderServiceImpl implements OrderService
 
         return orderSubmitVO;
     }
+
+    /**
+     * 魔改版本支付功能 直接付款成功
+     *
+     * @param ordersPaymentDTO
+     */
+    public void paySuccess(OrdersPaymentDTO ordersPaymentDTO) {
+
+        // 根据订单号查询订单
+        String orderNumber = ordersPaymentDTO.getOrderNumber();
+
+        // 根据订单id更新订单的状态、支付方式、支付状态、结账时间
+        Orders orders = Orders.builder()
+                .number(orderNumber) // 订单号
+                .status(Orders.TO_BE_CONFIRMED)
+                .payStatus(Orders.PAID)
+                .checkoutTime(LocalDateTime.now())
+                .build();
+
+        orderMapper.update(orders);
+    }
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+//    public OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+//        // 当前登录用户id
+//        Long userId = BaseContext.getCurrentId();
+//        User user = userMapper.getById(userId);
+//
+//        //调用微信支付接口，生成预支付交易单
+//        JSONObject jsonObject = weChatPayUtil.pay(
+//                ordersPaymentDTO.getOrderNumber(), //商户订单号
+//                new BigDecimal(0.01), //支付金额，单位 元
+//                "苍穹外卖订单", //商品描述
+//                user.getOpenid() //微信用户的openid
+//        );
+//
+//        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
+//            throw new OrderBusinessException("该订单已支付");
+//        }
+//
+//        OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
+//        vo.setPackageStr(jsonObject.getString("package"));
+//
+//        return vo;
+//    }
+
+//    /**
+//     * 支付成功，修改订单状态
+//     *
+//     * @param outTradeNo
+//     */
+//    public void paySuccess(String outTradeNo) {
+//
+//        // 根据订单号查询订单
+//        Orders ordersDB = orderMapper.getByNumber(outTradeNo);
+//
+//        // 根据订单id更新订单的状态、支付方式、支付状态、结账时间
+//        Orders orders = Orders.builder()
+//                .id(ordersDB.getId())
+//                .status(Orders.TO_BE_CONFIRMED)
+//                .payStatus(Orders.PAID)
+//                .checkoutTime(LocalDateTime.now())
+//                .build();
+//
+//        orderMapper.update(orders);
+//    }
 }
