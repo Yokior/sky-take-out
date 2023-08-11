b86f0f87629041a3bbfc5e91bb8774fc2a3c6310
Yokior <844599449@qq.com>
2023-08-11 14:43:36 +0800

    管理层_导出运营数据报表

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -5,14 +5,19 @@ import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
-import com.sky.vo.OrderReportVO;
-import com.sky.vo.SalesTop10ReportVO;
-import com.sky.vo.TurnoverReportVO;
-import com.sky.vo.UserReportVO;
+import com.sky.service.WorkspaceService;
+import com.sky.vo.*;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -29,6 +34,9 @@ public class ReportServiceImpl implements ReportService
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private WorkspaceService workspaceService;
 
     /**
      * 营业额数据统计
@@ -210,7 +218,7 @@ public class ReportServiceImpl implements ReportService
         LocalDateTime beginTime = LocalDateTime.of(begin, LocalTime.MIN);
         LocalDateTime endTime = LocalDateTime.of(end, LocalTime.MAX);
 
-        List<GoodsSalesDTO> goodsSalesDTOList = orderMapper.getSalesTop10(beginTime,endTime);
+        List<GoodsSalesDTO> goodsSalesDTOList = orderMapper.getSalesTop10(beginTime, endTime);
 
         List<String> stringList = goodsSalesDTOList.stream()
                 .map(GoodsSalesDTO::getName)
@@ -221,9 +229,87 @@ public class ReportServiceImpl implements ReportService
                 .collect(Collectors.toList());
 
         return SalesTop10ReportVO.builder()
-                .nameList(StringUtils.join(stringList,","))
-                .numberList(StringUtils.join(integerList,","))
+                .nameList(StringUtils.join(stringList, ","))
+                .numberList(StringUtils.join(integerList, ","))
                 .build();
+    }
+
+    /**
+     * 导出运营数据报表
+     *
+     * @param response
+     */
+    @Override
+    public void exportBusinessData(HttpServletResponse response)
+    {
+        // 查询数据库 获取营业数据 查询最近30天数据
+        LocalDate dateBegin = LocalDate.now().minusDays(30);
+        LocalDate dateEnd = LocalDate.now().minusDays(1);
+
+        LocalDateTime begin = LocalDateTime.of(dateBegin, LocalTime.MIN);
+        LocalDateTime end = LocalDateTime.of(dateEnd, LocalTime.MAX);
+
+        BusinessDataVO businessData = workspaceService.getBusinessData(begin, end);
+
+        // 通过POI将数据写入excel文件中
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("template/运营数据报表模板.xlsx");
+
+        try
+        {
+            XSSFWorkbook excel = new XSSFWorkbook(inputStream);
+            // 填充数据
+            XSSFSheet sheet = excel.getSheet("Sheet1");
+            // 时间
+            sheet.getRow(1).getCell(1).setCellValue("时间" + dateBegin + "至" + dateEnd);
+            // 获取第4行
+            XSSFRow row4 = sheet.getRow(3);
+            // 营业额
+            row4.getCell(2).setCellValue(businessData.getTurnover());
+            // 订单完成率
+            row4.getCell(4).setCellValue(businessData.getOrderCompletionRate());
+            // 新增用户
+            row4.getCell(6).setCellValue(businessData.getNewUsers());
+
+            // 获取第5行
+            XSSFRow row5 = sheet.getRow(4);
+            // 有效订单
+            row5.getCell(2).setCellValue(businessData.getValidOrderCount());
+            // 平均客单价
+            row5.getCell(4).setCellValue(businessData.getUnitPrice());
+
+            // 填充明细数据
+            for (int i = 0; i < 30; i++)
+            {
+                LocalDate date = dateBegin.plusDays(i);
+                // 查询某一天的营业数据
+                BusinessDataVO businessDataVO = workspaceService.getBusinessData(LocalDateTime.of(date, LocalTime.MIN), LocalDateTime.of(date, LocalTime.MAX));
+
+                XSSFRow row = sheet.getRow(7 + i);
+                row.getCell(1).setCellValue(date.toString());
+                row.getCell(2).setCellValue(businessDataVO.getTurnover());
+                row.getCell(3).setCellValue(businessDataVO.getValidOrderCount());
+                row.getCell(4).setCellValue(businessDataVO.getOrderCompletionRate());
+                row.getCell(5).setCellValue(businessDataVO.getUnitPrice());
+                Integer newUsers = businessDataVO.getNewUsers();
+                if (newUsers == null)
+                {
+                    newUsers = 0;
+                }
+                row.getCell(6).setCellValue(newUsers);
+            }
+
+            // 通过输出流将Excel文件下载到客户端浏览器
+            ServletOutputStream outputStream = response.getOutputStream();
+            excel.write(outputStream);
+
+            outputStream.close();
+            excel.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
     /**
